af57d17e2ab8c1342f96838c05eea35fb38d09b6
joashneves <joashneves@gmail.com>
2025-02-06 14:15:31 -0300

    apredendo c++

---
+++ lang/cpp/jogodaadivinhacao.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std; // Faz que não precise declarar `std` antes das funções
+
+int main(){
+
+  std::cout << "***************************************" << std::endl;
+  std::cout << "* Boas vindas ao Jogo da Adivinhação! *" << std::endl;
+  std::cout << "***************************************" << std::endl;
+
+}
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Repositorio para guarda coisas
 
 Este é um repositorio que vai ser usado para guardar testes pequenos a vulso, como dar hello world em alguma linguagem nova, ou salvar paginas estaticas, ou ate mesmo desafios aleatorios.
-Enjoe :D
+_Enjoy_ :D
 
 ## FrontEnd
 Pasta aonde estão minhas paginas estaticas
