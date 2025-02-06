524cf55466926a9850a94867844213c6204bd012
joashneves <joashneves@gmail.com>
2025-02-06 14:42:16 -0300

    coloca coisa

--- lang/cpp/jogodaadivinhacao.cpp
+++ lang/cpp/jogodaadivinhacao.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<iostream>
+
 using namespace std; // Faz que não precise declarar `std` antes das funções
 
 int main(){
@@ -12,18 +13,25 @@ int main(){
   const int NUMERO_SECRETO = 42;
   cout << "O numero secreto é " << NUMERO_SECRETO << ". Não conte a ninguem" << endl;
 
+  bool nao_acertou = true;
   int chute;
-  cout << "Qual valor do chute? ";
-  cin >> chute;
-  cout << "Valor do chute : " << chute << endl;
+  int tentativas = 0;
+  while(nao_acertou){
+    tentativas++;
+    cout << "Qual valor do chute? ";
+    cin >> chute;
+    cout << "Valor do chute : " << chute << endl;
 
-  bool acertou = chute == NUMERO_SECRETO;
-  if (acertou){
-    cout << "Parabens voce ganhou!";
-  }else if (chute > NUMERO_SECRETO){
-    cout << "Chute foi maior que numero secreto";
-  }else if ( chute < NUMERO_SECRETO){
-    cout << "Chute foi menor que numero secrevo";
+    bool acertou = chute == NUMERO_SECRETO;
+
+    if (acertou){
+      cout << "Parabens voce ganhou!";
+      nao_acertou = false
+    }else if (chute > NUMERO_SECRETO){
+      cout << "Chute foi maior que numero secreto";
+    }else if ( chute < NUMERO_SECRETO){
+      cout << "Chute foi menor que numero secrevo";
+    };
   };
 
 }
