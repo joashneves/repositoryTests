4e8e7ea42d4c189d3afe3b3cb7283fdd54980bb7
joashneves <joashneves@gmail.com>
2025-04-22 13:49:06 -0300

    aula 4

--- lang/biblioteca/src/index.js
+++ lang/biblioteca/src/index.js
@@ -1,19 +1,29 @@
 const fs = require('fs')
-
+const trataErro = require('./funcoesErro.js')
 const caminhoArgumento = process.argv;
 const link = caminhoArgumento[2]
 
-fs.readFile(link, 'utf-8', (error, texto) => {
-  quebraEmParagrafos(texto)
+fs.readFile(link, 'utf-8', (erro, texto) => {
+  try{
+  if (erro) throw erro;
+  contaPalavra(texto)
+} catch(erro){
+  trataErro(erro);
+}
 })
 
-function quebraEmParagrafos(texto){
-  const paragrafos = texto.toLowerCase().split('\r').join('').split('\n');
+function contaPalavra(texto){
+  const paragrafos = extraiParagrafos(texto)
   const contagem = paragrafos.flatMap((paragrafos)=>{
     if(!paragrafos) return []
     return verificaPalavraDuplicada(paragrafos)
   })
   console.log(contagem)
+}
+
+function extraiParagrafos(texto){
+  return texto.toLowerCase().split('\r').join('').split('\n');
+
 }
 
 function limparPalavra(palavra){
