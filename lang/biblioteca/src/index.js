7fda7f926887c4e02a54655c027ac52a1ea7a8cb
joashneves <joashneves@gmail.com>
2025-04-22 13:09:38 -0300

    aula 2 - biblioteca

--- lang/biblioteca/src/index.js
+++ lang/biblioteca/src/index.js
@@ -4,7 +4,30 @@ const caminhoArgumento = process.argv;
 const link = caminhoArgumento[2]
 
 fs.readFile(link, 'utf-8', (error, texto) => {
-  console.log(texto)
+  quebraEmParagrafos(texto)
 })
 
-console.log(link)
+function quebraEmParagrafos(texto){
+  const paragrafos = texto.toLowerCase().split('\r').join('').split('\n');
+  const contagem = paragrafos.flatMap((paragrafos)=>{
+    if(!paragrafos) return []
+    return verificaPalavraDuplicada(paragrafos)
+  })
+  console.log(contagem)
+}
+
+function limparPalavra(palavra){
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
+
+function verificaPalavraDuplicada(texto){
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  listaPalavras.forEach(palavra => {
+    if (palavra.length >= 3){
+      const palavraLimpa = limparPalavra(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] ||  0) + 1 
+    }
+  });
+  return resultado
+}
